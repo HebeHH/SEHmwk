16863b96ed1a5af599129b45a59365bf8380f38f
Hebe Hilhorst <hebehh@gmail.com>
2019-08-23 12:52:06 +0800

    readme added

---
+++ README.ml
@@ -0,0 +1,11 @@
+# Software Engineering
+
+## Description
+
+This is the codebase for my YSC3232 Software Engineering homework, in Java.
+
+## Contents
+
+### Week 2
+
+Summary statistics: create a java program that takes in *n* ints and prints the min, max and average.
